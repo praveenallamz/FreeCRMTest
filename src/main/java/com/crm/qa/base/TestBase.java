dac523d4d72d2c8fc99116fd6dba3da87d0657b2
Praveen <Praveen@192.168.0.203>
2018-06-27 15:42:26 +0530

    test

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,11 +41,11 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Praveen\\Desktop\\Docs\\chromedriver.exe");	
+			System.setProperty("webdriver.chrome.driver", "C:/Users/Praveen/Desktop/Docs/chromedriver.exe");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Praveen\\Desktop\\Docs\\exegeckodriver.exe");	
+			System.setProperty("webdriver.gecko.driver", "C:/Users/Praveen/Desktop/Docs/exegeckodriver.exe");	
 			driver = new FirefoxDriver(); 
 		}
 		
