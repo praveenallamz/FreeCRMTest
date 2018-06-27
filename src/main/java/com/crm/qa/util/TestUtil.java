92870e4893139adade318d39d95ca5bcc03606ff
Praveen <Praveen@192.168.0.203>
2018-06-27 15:39:44 +0530

    test

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -20,10 +20,9 @@ public class TestUtil extends TestBase{
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 	
-	public static String TESTDATA_SHEET_PATH = "C:\\Users\\Praveen\\Desktop\\Docs\\FreeCRMTest-master\\FreeCRMTest-master\\src\\main\\java\\com\\crm\\qa\\testdata\\FreeCrmTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = " https:\\github.com\\praveenallamz\\FreeCRMTest.git\\FreeCrmTestData.xlsx";
 	
-	
-	
+		
 	static Workbook book;
 	static Sheet sheet;
 	
